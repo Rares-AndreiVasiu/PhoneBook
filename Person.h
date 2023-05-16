9dfe3d9562cb0f26591a9b8e441b87adec2e4101
Rares-Andrei Vasiu <rares_vasiu03@yahoo.ro>
2023-05-16 23:24:22 +0300

    added the Person class to main

---
+++ Person.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "Address.h"
+#include "Phone.h"
+
+class Person
+{
+private:
+    std::string firstName;
+    std::string lastName;
+
+    int age;
+
+    Phone *phone;
+    Address *address;
+
+public:
+    Person(const std::string& first, const std::string& last, int a, 
+    Phone *p, Address *adr) : firstName(first), lastName(last), age(a),
+    phone(p), address(adr)
+    {
+
+    }
+
+    std::string getFirstName() const;
+
+    std::string getLastName() const;
+
+    int getAge();
+
+    Phone *getPhone() const;
+
+    Address *getAddress() const;
+};
+
