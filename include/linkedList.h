1d1fa1f4ebafa85731b15792fc858709e2d0c413
Rares-Andrei Vasiu <rares_vasiu03@yahoo.ro>
2023-05-31 18:45:18 +0300

    modified linkedList to handle bad user input

---
+++ include/linkedList.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <iostream>
+#include "PhoneBook.h"
+
+typedef struct _node
+{
+    PhoneBook *phoneBookObj;
+
+    struct _node *next;
+} node;
+
+class linkedList
+{
+private:
+    node *head;
+
+public:
+    // Constructor
+    linkedList();
+
+    // Destructor
+    ~linkedList()
+    {
+        deleteList();
+    }
+
+    // Function to add a new node at the end of the list
+    void addNode(PhoneBook *data);
+
+    // Function to delete the entire list
+    void deleteList();
+
+    // // Function to search for a given value in the list
+    // bool search(int value)
+    // {
+    //     node *current = head;
+    //     while (current != nullptr)
+    //     {
+    //         if (current->data == value)
+    //         {
+    //             return true;
+    //         }
+    //         current = current->next;
+    //     }
+    //     return false;
+    // }
+
+    // Function to print the list
+    void printList();
+};
