e15f32c74bf4392847b7043e53e98f7b497e0ed3
Rares-Andrei Vasiu <rares_vasiu03@yahoo.ro>
2023-05-31 11:57:01 +0300

    added the build folder

---
+++ build/src/CMakeFiles/register.x.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/rares/GitHub/PhoneBook/src/Address.cpp" "src/CMakeFiles/register.x.dir/Address.o" "gcc" "src/CMakeFiles/register.x.dir/Address.o.d"
+  "/mnt/c/Users/rares/GitHub/PhoneBook/src/Person.cpp" "src/CMakeFiles/register.x.dir/Person.o" "gcc" "src/CMakeFiles/register.x.dir/Person.o.d"
+  "/mnt/c/Users/rares/GitHub/PhoneBook/src/Phone.cpp" "src/CMakeFiles/register.x.dir/Phone.o" "gcc" "src/CMakeFiles/register.x.dir/Phone.o.d"
+  "/mnt/c/Users/rares/GitHub/PhoneBook/src/PhoneBook.cpp" "src/CMakeFiles/register.x.dir/PhoneBook.o" "gcc" "src/CMakeFiles/register.x.dir/PhoneBook.o.d"
+  "/mnt/c/Users/rares/GitHub/PhoneBook/src/main.cpp" "src/CMakeFiles/register.x.dir/main.o" "gcc" "src/CMakeFiles/register.x.dir/main.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/register.x.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/register.x.dir/Address.o"
+  "CMakeFiles/register.x.dir/Address.o.d"
+  "CMakeFiles/register.x.dir/Person.o"
+  "CMakeFiles/register.x.dir/Person.o.d"
+  "CMakeFiles/register.x.dir/Phone.o"
+  "CMakeFiles/register.x.dir/Phone.o.d"
+  "CMakeFiles/register.x.dir/PhoneBook.o"
+  "CMakeFiles/register.x.dir/PhoneBook.o.d"
+  "CMakeFiles/register.x.dir/main.o"
+  "CMakeFiles/register.x.dir/main.o.d"
+  "register.x"
+  "register.x.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/register.x.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
